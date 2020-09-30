12f3f7647af35d7bea2296859fdfadf559d98d92
Phillip Kemkes <phillip.kemkes@gdata.de>
2020-09-30 13:39:14 +0200

    Add example service for metrics usage.
    Part of #13.

---
+++ examples/Metrics/DifferentNamespace/ServiceInDifferentNamespace.cs
@@ -0,0 +1,27 @@
+﻿using Motor.Extensions.Diagnostics.Metrics.Abstractions;
+using Prometheus.Client;
+using Prometheus.Client.Abstractions;
+
+namespace Metrics.DifferentNamespace
+{
+    public interface IServiceInDifferentNamespace
+    {
+        void CountInDifferentNamespace();
+    }
+
+    public class ServiceInDifferentNamespace : IServiceInDifferentNamespace
+    {
+        private readonly IMetricFamily<ICounter>? _counter;
+
+        public ServiceInDifferentNamespace(IMetricsFactory<ServiceInDifferentNamespace>? metricsFactory)
+        {
+            // Resulting label in Prometheus: metrics_differentnamespace_counter_in_different_namespace
+            _counter = metricsFactory?.CreateCounter("counter_in_different_namespace", "This counts something else.");
+        }
+
+        public void CountInDifferentNamespace()
+        {
+            _counter?.Inc();
+        }
+    }
+}
---
+++ examples/Metrics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Metrics.DifferentNamespace;
+using Metrics.Model;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Motor.Extensions.Conversion.SystemJson;
+using Motor.Extensions.Hosting.Abstractions;
+using Motor.Extensions.Hosting.Consumer;
+using Motor.Extensions.Hosting.RabbitMQ;
+using Motor.Extensions.Utilities;
+
+namespace Metrics
+{
+    public static class Program
+    {
+        public static Task Main() =>
+            MotorHost.CreateDefaultBuilder()
+                // Configure the types of the input and output messages
+                .ConfigureNoOutputService<InputMessage>()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // Add a handler for the input message which returns an output message
+                    // This handler is called for every new incoming message
+                    services.AddTransient<INoOutputService<InputMessage>, ServiceWithMetrics>();
+                    services.AddTransient<IServiceInDifferentNamespace, ServiceInDifferentNamespace>();
+                })
+                // Add the incomming communication module. 
+                .ConfigureConsumer<InputMessage>((context, builder) =>
+                {
+                    // In this case the messages are received from RabbitMQ
+                    builder.AddRabbitMQ();
+                    // The encoding of the incoming message, such that the handler is able to deserialize the message
+                    builder.AddSystemJson();
+                })
+                .RunConsoleAsync();
+    }
+}
