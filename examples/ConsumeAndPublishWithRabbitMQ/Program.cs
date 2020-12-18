9387aab778e82155d5ff4efbeab1ae88ca499ae8
jan.jansen <jan.jansen@gdata.de>
2020-12-18 12:42:09 +0100

    Use c#9 top level statement
    Signed-off-by: jan.jansen <jan.jansen@gdata.de>

--- examples/ConsumeAndPublishWithRabbitMQ/Program.cs
+++ examples/ConsumeAndPublishWithRabbitMQ/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Tasks;
+﻿using ConsumeAndPublishWithRabbitMQ;
 using ConsumeAndPublishWithRabbitMQ.Model;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,36 +9,29 @@ using Motor.Extensions.Hosting.Publisher;
 using Motor.Extensions.Hosting.RabbitMQ;
 using Motor.Extensions.Utilities;
 
-namespace ConsumeAndPublishWithRabbitMQ
-{
-    public static class Program
+await MotorHost.CreateDefaultBuilder()
+    // Configure the types of the input and output messages
+    .ConfigureSingleOutputService<InputMessage, OutputMessage>()
+    .ConfigureServices((_, services) =>
     {
-        public static Task Main() =>
-            MotorHost.CreateDefaultBuilder()
-                // Configure the types of the input and output messages
-                .ConfigureSingleOutputService<InputMessage, OutputMessage>()
-                .ConfigureServices((_, services) =>
-                {
-                    // Add a handler for the input message which returns an output message
-                    // This handler is called for every new incoming message
-                    services.AddTransient<ISingleOutputService<InputMessage, OutputMessage>, SingleOutputService>();
-                })
-                // Add the incomming communication module. 
-                .ConfigureConsumer<InputMessage>((_, builder) =>
-                {
-                    // In this case the messages are received from RabbitMQ
-                    builder.AddRabbitMQ();
-                    // The encoding of the incoming message, such that the handler is able to deserialize the message
-                    builder.AddSystemJson();
-                })
-                // Add the outgoing communication module.
-                .ConfigurePublisher<OutputMessage>((_, builder) =>
-                {
-                    // In this case the messages are send to RabbitMQ
-                    builder.AddRabbitMQ();
-                    // The encoding of the outgoing message, such that the handler is able to serialize the message
-                    builder.AddSystemJson();
-                })
-                .RunConsoleAsync();
-    }
-}
+        // Add a handler for the input message which returns an output message
+        // This handler is called for every new incoming message
+        services.AddTransient<ISingleOutputService<InputMessage, OutputMessage>, SingleOutputService>();
+    })
+    // Add the incomming communication module. 
+    .ConfigureConsumer<InputMessage>((_, builder) =>
+    {
+        // In this case the messages are received from RabbitMQ
+        builder.AddRabbitMQ();
+        // The encoding of the incoming message, such that the handler is able to deserialize the message
+        builder.AddSystemJson();
+    })
+    // Add the outgoing communication module.
+    .ConfigurePublisher<OutputMessage>((_, builder) =>
+    {
+        // In this case the messages are send to RabbitMQ
+        builder.AddRabbitMQ();
+        // The encoding of the outgoing message, such that the handler is able to serialize the message
+        builder.AddSystemJson();
+    })
+    .RunConsoleAsync();
