e7c72322f2614a6ddbfa6b9d3e23158c668d3274
jan.jansen <jan.jansen@gdata.de>
2020-12-18 12:42:09 +0100

    Use c#9 top level statement
    Signed-off-by: jan.jansen <jan.jansen@gdata.de>

--- examples/Metrics/Program.cs
+++ examples/Metrics/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Tasks;
+﻿using Metrics;
 using Metrics.DifferentNamespace;
 using Metrics.Model;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,29 +9,22 @@ using Motor.Extensions.Hosting.Consumer;
 using Motor.Extensions.Hosting.RabbitMQ;
 using Motor.Extensions.Utilities;
 
-namespace Metrics
-{
-    public static class Program
+await MotorHost.CreateDefaultBuilder()
+    // Configure the types of the input and output messages
+    .ConfigureNoOutputService<InputMessage>()
+    .ConfigureServices((hostContext, services) =>
     {
-        public static Task Main() =>
-            MotorHost.CreateDefaultBuilder()
-                // Configure the types of the input and output messages
-                .ConfigureNoOutputService<InputMessage>()
-                .ConfigureServices((hostContext, services) =>
-                {
-                    // Add a handler for the input message which returns an output message
-                    // This handler is called for every new incoming message
-                    services.AddTransient<INoOutputService<InputMessage>, ServiceWithMetrics>();
-                    services.AddTransient<IServiceInDifferentNamespace, ServiceInDifferentNamespace>();
-                })
-                // Add the incomming communication module. 
-                .ConfigureConsumer<InputMessage>((context, builder) =>
-                {
-                    // In this case the messages are received from RabbitMQ
-                    builder.AddRabbitMQ();
-                    // The encoding of the incoming message, such that the handler is able to deserialize the message
-                    builder.AddSystemJson();
-                })
-                .RunConsoleAsync();
-    }
-}
+        // Add a handler for the input message which returns an output message
+        // This handler is called for every new incoming message
+        services.AddTransient<INoOutputService<InputMessage>, ServiceWithMetrics>();
+        services.AddTransient<IServiceInDifferentNamespace, ServiceInDifferentNamespace>();
+    })
+    // Add the incomming communication module. 
+    .ConfigureConsumer<InputMessage>((context, builder) =>
+    {
+        // In this case the messages are received from RabbitMQ
+        builder.AddRabbitMQ();
+        // The encoding of the incoming message, such that the handler is able to deserialize the message
+        builder.AddSystemJson();
+    })
+    .RunConsoleAsync();
