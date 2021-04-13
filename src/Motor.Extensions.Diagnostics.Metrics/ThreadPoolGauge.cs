529a042d312a628220da6edf3200779f2c8703c8
baez90 <peter.kurfer@googlemail.com>
2021-04-13 16:05:14 +0200

    Make global ThreadPool configurable (#180)
    - allow to override minimum worker and completion port threads to avoid (containerized) scenarios where applications are suddenly 'single-threaded' when the .NET runtime detects a single host CPU due to configured resource limits
    - export metrics of the global thread pool to allow further investigations of performance bottlenecks

---
+++ src/Motor.Extensions.Diagnostics.Metrics/ThreadPoolGauge.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.Threading;
+using Prometheus.Client;
+using Prometheus.Client.Collectors;
+using Prometheus.Client.MetricsWriter;
+
+namespace Motor.Extensions.Diagnostics.Metrics
+{
+    internal class ThreadPoolGauge : ICollector
+    {
+        private static readonly string[] Labels =
+        {
+            "ThreadPoolOption"
+        };
+
+        internal static readonly MetricConfiguration ThreadPoolGaugeConfig = new(
+            "ThreadPoolState",
+            "Expose information about the internal .NET global ThreadPool",
+            Labels,
+            false
+        );
+
+        public void Collect(IMetricsWriter writer)
+        {
+            ThreadPool.GetMaxThreads(out var maxWorkerThreads, out var maxCompletionThreads);
+            ThreadPool.GetMinThreads(out var minWorkerThreads, out var minCompletionThreads);
+            ThreadPool.GetAvailableThreads(out var availableWorkerThreads, out var availableCompletionThreads);
+
+            WriteSample(writer, minWorkerThreads, nameof(minWorkerThreads));
+            WriteSample(writer, minCompletionThreads, nameof(minCompletionThreads));
+            WriteSample(writer, maxWorkerThreads, nameof(maxWorkerThreads));
+            WriteSample(writer, maxCompletionThreads, nameof(maxCompletionThreads));
+            WriteSample(writer, ThreadPool.ThreadCount, nameof(ThreadPool.ThreadCount));
+            WriteSample(writer, availableWorkerThreads, nameof(availableWorkerThreads));
+            WriteSample(writer, availableCompletionThreads, nameof(availableCompletionThreads));
+        }
+
+        public CollectorConfiguration Configuration => ThreadPoolGaugeConfig;
+
+        public IReadOnlyList<string> MetricNames => new[] { ThreadPoolGaugeConfig.Name };
+
+        private void WriteSample(IMetricsWriter writer, double value, string labelValue)
+        {
+            writer.WriteSample(
+                value,
+                string.Empty,
+                Labels,
+                new[] { Capitalize(labelValue) });
+        }
+
+        private static string Capitalize(string labelValue) =>
+            char.ToUpperInvariant(labelValue[0]) + labelValue.Substring(1);
+    }
+}
---
+++ src/Motor.Extensions.Utilities/ThreadPoolSetupService.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace Motor.Extensions.Utilities
+{
+    public record ThreadPoolOptions
+    {
+        public int MinWorkerThreads { get; init; } = Environment.ProcessorCount;
+        public int MinCompletionThreads { get; init; } = Environment.ProcessorCount;
+    }
+
+    public sealed class ThreadPoolSetupService : IHostedService
+    {
+        private readonly IOptions<ThreadPoolOptions> _threadPoolOptions;
+
+
+        public ThreadPoolSetupService(IOptions<ThreadPoolOptions> threadPoolOptions)
+        {
+            _threadPoolOptions = threadPoolOptions;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken = default)
+        {
+            SetupGlobalThreadPool(_threadPoolOptions.Value.MinWorkerThreads,
+                _threadPoolOptions.Value.MinCompletionThreads);
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken = default) => Task.CompletedTask;
+
+        private static void SetupGlobalThreadPool(int desiredMinWorkerThreads, int desiredMinCompletionThreads)
+        {
+            if (desiredMinWorkerThreads <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(desiredMinWorkerThreads), desiredMinWorkerThreads, "Minimum number of worker threads must be positive");
+            }
+
+            if (desiredMinCompletionThreads <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(desiredMinCompletionThreads), desiredMinCompletionThreads, "Minimum number of completion port threads must be positive");
+            }
+
+            ThreadPool.GetMinThreads(out var minWorkerThreads, out var minCompletionThreads);
+            ThreadPool.GetMaxThreads(out var maxWorkerThreads, out var maxCompletionThreads);
+
+            desiredMinWorkerThreads = Math.Min(desiredMinWorkerThreads, maxWorkerThreads);
+            desiredMinCompletionThreads = Math.Min(desiredMinCompletionThreads, maxCompletionThreads);
+
+            desiredMinWorkerThreads = Math.Max(minWorkerThreads, desiredMinWorkerThreads);
+            desiredMinCompletionThreads = Math.Max(minCompletionThreads, desiredMinCompletionThreads);
+            ThreadPool.SetMinThreads(desiredMinWorkerThreads, desiredMinCompletionThreads);
+        }
+    }
+}
