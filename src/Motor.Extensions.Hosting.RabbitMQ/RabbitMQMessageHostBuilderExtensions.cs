e740c8487211cbb9f2fc6e2c910715ac74dc3b50
rngcntr <7890887+rngcntr@users.noreply.github.com>
2021-08-06 07:38:29 +0200

    Allow multiple RabbitMQ connections with different configs (#285)
    * Allow multiple RabbitMQ connections with different configs
    
    * Add tests
    
    * Apply nitpicks
    
    Co-authored-by: Phillip Kemkes <phillip.kemkes@gdata.de>

--- src/Motor.Extensions.Hosting.RabbitMQ/RabbitMQMessageHostBuilderExtensions.cs
+++ src/Motor.Extensions.Hosting.RabbitMQ/RabbitMQMessageHostBuilderExtensions.cs
@@ -2,10 +2,13 @@ using CloudNative.CloudEvents;
 using CloudNative.CloudEvents.SystemTextJson;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Motor.Extensions.Diagnostics.Queue.Abstractions;
 using Motor.Extensions.Hosting.Abstractions;
+using Motor.Extensions.Hosting.CloudEvents;
 using Motor.Extensions.Hosting.RabbitMQ.Options;
+using MSOptions = Microsoft.Extensions.Options.Options;
 
 namespace Motor.Extensions.Hosting.RabbitMQ
 {
@@ -14,11 +17,18 @@ namespace Motor.Extensions.Hosting.RabbitMQ
         public static void AddRabbitMQWithConfig<T>(this IConsumerBuilder<T> builder, IConfiguration config) where T : notnull
         {
             builder.AddTransient<CloudEventFormatter, JsonEventFormatter>();
-            builder.Configure<RabbitMQConsumerOptions<T>>(config);
-            builder.AddSingleton(sp =>
-                RabbitMQConnectionFactory<T>.From(sp.GetRequiredService<IOptions<RabbitMQConsumerOptions<T>>>().Value));
-            builder.AddConsumer<RabbitMQMessageConsumer<T>>();
-            builder.AddSingleton<IQueueMonitor, RabbitMQQueueMonitor<T>>();
+            var consumerOptions = new RabbitMQConsumerOptions<T>();
+            config.Bind(consumerOptions);
+            var connectionFactory = RabbitMQConnectionFactory<T>.From(consumerOptions);
+            builder.AddConsumer(sp => new RabbitMQMessageConsumer<T>(
+                sp.GetRequiredService<ILogger<RabbitMQMessageConsumer<T>>>(),
+                connectionFactory,
+                MSOptions.Create(consumerOptions),
+                sp.GetRequiredService<IHostApplicationLifetime>(),
+                sp.GetRequiredService<IApplicationNameService>()));
+            builder.AddSingleton<IQueueMonitor>(sp =>
+                new RabbitMQQueueMonitor<T>(sp.GetRequiredService<ILogger<RabbitMQQueueMonitor<T>>>(),
+                    MSOptions.Create(consumerOptions), connectionFactory));
         }
 
         public static void AddRabbitMQ<T>(this IConsumerBuilder<T> builder, string configSection = "RabbitMQConsumer") where T : notnull
@@ -30,10 +40,13 @@ namespace Motor.Extensions.Hosting.RabbitMQ
         {
             builder.AddTransient<CloudEventFormatter, JsonEventFormatter>();
             builder.Configure<RabbitMQPublisherOptions<T>>(config);
-            builder.AddSingleton(sp =>
-                RabbitMQConnectionFactory<T>.From(sp.GetRequiredService<IOptions<RabbitMQPublisherOptions<T>>>()
-                    .Value));
-            builder.AddPublisher<RabbitMQMessagePublisher<T>>();
+            var publisherOptions = new RabbitMQPublisherOptions<T>();
+            config.Bind(publisherOptions);
+            var connectionFactory = RabbitMQConnectionFactory<T>.From(publisherOptions);
+            builder.AddPublisher(sp
+             => new RabbitMQMessagePublisher<T>(connectionFactory,
+                 MSOptions.Create(publisherOptions),
+                 sp.GetRequiredService<CloudEventFormatter>()));
         }
 
         public static void AddRabbitMQ<T>(this IPublisherBuilder<T> builder, string configSection = "RabbitMQPublisher") where T : notnull
