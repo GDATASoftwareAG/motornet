d3a5c12fca604175871c5ff59807d594008771ef
Jan Jansen <jan.jansen@gdata.de>
2021-11-11 15:55:58 +0100

    some refactoring

--- src/Motor.Extensions.Hosting.Abstractions/CloudEventHandlingOptions.cs
+++ src/Motor.Extensions.Hosting.Abstractions/CloudEventHandlingOptions.cs
@@ -2,7 +2,13 @@ namespace Motor.Extensions.Hosting.Abstractions;
 
 public enum CloudEventFormat
 {
+    /// <summary>
+    /// If supported by publisher or consumer, the protocol format uses headers from Kafka or RabbitMQ to store CloudEvent metadata.
+    /// </summary>
     Protocol,
+    /// <summary>
+    /// If supported by publisher or consumer, CloudEvent published in the enveloped variant, see https://github.com/cloudevents/spec/blob/v1.0.1/json-format.md#3-envelope.
+    /// </summary>
     Json,
 }
 public record PublisherOptions
