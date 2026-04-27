61a569f49447e0d53cb8f0d334ecd1a102bb6a2d
Philip Stadermann <philip.stadermann@gdata.de>
2026-04-27 12:09:39 +0200

    First implementation of producer

--- src/Motor.Extensions.Hosting.PgMq/Options/PgMqPublisherOptions.cs
+++ src/Motor.Extensions.Hosting.PgMq/Options/PgMqPublisherOptions.cs
@@ -2,4 +2,6 @@ namespace Motor.Extensions.Hosting.PgMq.Options;
 
 public class PgMqPublisherOptions<T>
 {
+    public string ConnectionString { get; set; } = string.Empty;
+    public string QueueName { get; set; } = string.Empty;
 }
--- src/Motor.Extensions.Hosting.PgMq/PgMqMessageProducer.cs
+++ src/Motor.Extensions.Hosting.PgMq/PgMqMessageProducer.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using CloudNative.CloudEvents;
+using Microsoft.Extensions.Options;
 using Motor.Extensions.Hosting.Abstractions;
 using Motor.Extensions.Hosting.CloudEvents;
+using Motor.Extensions.Hosting.PgMq.Options;
+using Npgmq;
 
 namespace Motor.Extensions.Hosting.PgMq;
 
@@ -18,20 +23,20 @@ namespace Motor.Extensions.Hosting.PgMq;
 public class PgMqMessageProducer<TOutput> : IRawMessagePublisher<TOutput>
     where TOutput : notnull
 {
-    /// <summary>
-    /// Publishes a MotorCloudEvent to a pgmq queue.
-    /// </summary>
-    /// <param name="motorCloudEvent"></param>
-    /// <param name="token"></param>
-    /// <returns>A Task that completes when the motorCloudEvent has been added to the DB.</returns>
-    /// <remarks>
-    /// <list>
-    /// <item>Publishes headers and data of the motorCloudEvents.</item>
-    /// </list>
-    /// </remarks>
-    public Task PublishMessageAsync(MotorCloudEvent<byte[]> motorCloudEvent, CancellationToken token = default)
+    private readonly PgMqPublisherOptions<TOutput> _options;
+    private readonly PublisherOptions _publisherOptions;
+    private readonly CloudEventFormatter _cloudEventFormatter;
+    private NpgmqClient? _npgmqClient;
+
+    public PgMqMessageProducer(
+        IOptions<PgMqPublisherOptions<TOutput>> options,
+        IOptions<PublisherOptions> publisherOptions,
+        CloudEventFormatter cloudEventFormatter
+    )
     {
-        throw new System.NotImplementedException();
+        _options = options.Value;
+        _publisherOptions = publisherOptions.Value;
+        _cloudEventFormatter = cloudEventFormatter;
     }
 
     /// <summary>
@@ -44,13 +49,59 @@ public class PgMqMessageProducer<TOutput> : IRawMessagePublisher<TOutput>
     /// * Ensures the pgmq extension has been created in postgres
     /// * Creates the queue
     /// </remarks>
-    public Task StartAsync(CancellationToken token = default)
+    public async Task StartAsync(CancellationToken token = default)
     {
-        // var npgmq = new NpgmqClient("<YOUR CONNECTION STRING HERE>");
-        //
-        // await npgmq.InitAsync(); // Optional ()
-        //
-        // await npgmq.CreateQueueAsync("my_queue");
-        throw new NotImplementedException();
+        _npgmqClient = new NpgmqClient(_options.ConnectionString);
+        await _npgmqClient.InitAsync(token);
+        await _npgmqClient.CreateQueueAsync(_options.QueueName, token);
+    }
+
+    /// <summary>
+    /// Publishes a MotorCloudEvent to a pgmq queue.
+    /// </summary>
+    /// <param name="motorCloudEvent"></param>
+    /// <param name="token"></param>
+    /// <returns>A Task that completes when the motorCloudEvent has been added to the DB.</returns>
+    /// <remarks>
+    /// <list>
+    /// <item>Publishes headers and data of the motorCloudEvents.</item>
+    /// </list>
+    /// </remarks>
+    public async Task PublishMessageAsync(MotorCloudEvent<byte[]> motorCloudEvent, CancellationToken token = default)
+    {
+        if (_npgmqClient is null)
+        {
+            throw new InvalidOperationException("Producer is not started. Call StartAsync first.");
+        }
+
+        switch (_publisherOptions.CloudEventFormat)
+        {
+            case CloudEventFormat.Protocol:
+                var headers = BuildHeaders(motorCloudEvent);
+                await _npgmqClient.SendAsync(_options.QueueName, motorCloudEvent.TypedData, headers, token);
+                break;
+            case CloudEventFormat.Json:
+                var jsonBytes = _cloudEventFormatter.EncodeStructuredModeMessage(
+                    motorCloudEvent.ConvertToCloudEvent(),
+                    out _
+                );
+                var json = Encoding.UTF8.GetString(jsonBytes.ToArray());
+                await _npgmqClient.SendAsync(_options.QueueName, json, token);
+                break;
+            default:
+                throw new UnhandledCloudEventFormatException(_publisherOptions.CloudEventFormat);
+        }
+    }
+
+    private static System.Collections.Generic.IReadOnlyDictionary<string, object> BuildHeaders(
+        MotorCloudEvent<byte[]> motorCloudEvent
+    )
+    {
+        var headers = new System.Collections.Generic.Dictionary<string, object>();
+        foreach (var (attribute, value) in motorCloudEvent.GetPopulatedAttributes())
+        {
+            headers[attribute.Name] = attribute.Format(value);
+        }
+        return headers;
     }
 }
