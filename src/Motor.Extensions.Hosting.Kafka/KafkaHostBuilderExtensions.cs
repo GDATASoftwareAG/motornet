8ed6666b391e5c888d0adb89584f77ec55116674
Philip Stadermann <philip.stadermann@gdata.de>
2026-04-27 16:45:53 +0200

    First implementation of Kafka DLQ

--- src/Motor.Extensions.Hosting.Kafka/KafkaHostBuilderExtensions.cs
+++ src/Motor.Extensions.Hosting.Kafka/KafkaHostBuilderExtensions.cs
@@ -2,6 +2,8 @@ using CloudNative.CloudEvents;
 using CloudNative.CloudEvents.SystemTextJson;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
 using Motor.Extensions.Hosting.Abstractions;
 using Motor.Extensions.Hosting.Kafka.Options;
 
@@ -21,6 +23,34 @@ public static class KafkaHostBuilderExtensions
         where T : notnull
     {
         builder.AddKafkaWithConfig(builder.Context.Configuration.GetSection(configSection));
+    }
+
+    /// <summary>
+    /// Registers a Kafka publisher as the dead letter queue for consumed messages of type <typeparamref name="T"/>.
+    /// Failed messages (<see cref="Motor.Extensions.Hosting.Abstractions.ProcessedMessageStatus.Failure"/>,
+    /// <see cref="Motor.Extensions.Hosting.Abstractions.ProcessedMessageStatus.InvalidInput"/>, and
+    /// <see cref="Motor.Extensions.Hosting.Abstractions.ProcessedMessageStatus.TemporaryFailure"/> after retries)
+    /// will be forwarded to the configured Kafka topic instead of being dropped or stopping the application.
+    /// </summary>
+    public static void AddKafkaDeadLetterQueueWithConfig<T>(this IConsumerBuilder<T> builder, IConfiguration config)
+        where T : notnull
+    {
+        builder.Configure<KafkaPublisherOptions<T>>(config);
+        builder.TryAddSingleton<IOptions<PublisherOptions>>(_ => Microsoft.Extensions.Options.Options.Create(new PublisherOptions()));
+        builder.AddSingleton<IRawMessagePublisher<T>, KafkaMessagePublisher<T>>();
+    }
+
+    /// <summary>
+    /// Registers a Kafka publisher as the dead letter queue for consumed messages of type <typeparamref name="T"/>,
+    /// reading publisher configuration from the <paramref name="configSection"/> key.
+    /// </summary>
+    public static void AddKafkaDeadLetterQueue<T>(
+        this IConsumerBuilder<T> builder,
+        string configSection = "KafkaDeadLetterQueue"
+    )
+        where T : notnull
+    {
+        builder.AddKafkaDeadLetterQueueWithConfig<T>(builder.Context.Configuration.GetSection(configSection));
     }
 
     public static void AddKafkaWithConfig<T>(this IPublisherBuilder<T> builder, IConfiguration config)
