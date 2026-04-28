bbc188d5044117a1527ac16631aa4844a557661b
Kevin Heise <kevin.heise@gdata.de>
2026-04-28 11:15:40 +0200

    dotnet csharpier also for tests

--- test/Motor.Extensions.Hosting.PgMq_IntegrationTest/PostgresContainer.cs
+++ test/Motor.Extensions.Hosting.PgMq_IntegrationTest/PostgresContainer.cs
@@ -1,5 +1,6 @@
 using DotNet.Testcontainers.Configurations;
 using DotNet.Testcontainers.Containers;
+
 namespace Motor.Extensions.Hosting.PgMq_IntegrationTest;
 
 public class PostgresContainer(IContainerConfiguration configuration) : DockerContainer(configuration);
